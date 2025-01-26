22aba8930a757dc24f21f7273671bbade4a495bb
Nathan Paul <mail@polarhive.net>
2025-01-26 17:44:43 +0530

    feat: goroutines

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 
 	"github.com/polarhive/wombat/db"
 	"github.com/polarhive/wombat/fetch"
 )
 
-const seedURL = "https://en.wikipedia.org/wiki/Palm_nut"
-const cooldown = 50
+const (
+	seedURL       = "https://en.wikipedia.org/wiki/Palm_nut"
+	// seedURL       = "https://en.wikipedia.org/wiki/Wombat"
+	maxGoroutines = 5
+	timeout       = 5
+)
 
 func main() {
 	log.Println("Initializing database...")
@@ -41,12 +46,23 @@ func fetchAndStoreLinks(url string) {
 	log.Printf("Storing page '%s' into the database\n", pageName)
 	db.SaveNode(pageName)
 
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, maxGoroutines)
+
 	for _, link := range links {
-		log.Printf("Storing link: %s\n", link)
-		db.SaveNode(link)
-		db.SaveLink(pageName, link)
-		time.Sleep(cooldown * time.Millisecond)
+		wg.Add(1)
+		semaphore <- struct{}{}
+
+		go func(link string) {
+			defer wg.Done()
+			log.Printf("Storing link: %s\n", link)
+			db.SaveNode(link)
+			db.SaveLink(pageName, link)
+			time.Sleep(timeout * time.Millisecond)
+			<-semaphore
+		}(link)
 	}
 
+	wg.Wait()
 	log.Printf("Finished processing links for page: %s\n", pageName)
 }
